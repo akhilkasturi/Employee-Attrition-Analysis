bd2fb7f0937061a81dee968b0a0f6b1ec3493ff6
Henry Nguyen <tuo91182@temple.edu>
2024-12-05 20:00:06 -0500

    added data set page

--- attrition_prediction/src/app/page.tsx
+++ attrition_prediction/src/app/page.tsx
@@ -1,18 +1,29 @@
 'use client'
 import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react"; // Add this import
 
 export default function Home() {
-
   const router = useRouter();
+  const [isClient, setIsClient] = useState(false);
+
+  // only renders after client-side mount
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
   const handlePredictionClick = () => {
+    router.prefetch('/Prediction'); // load in the background before user clicks
     router.push('/Prediction');
   }
 
+  // If not client-side, return null
+  if (!isClient) {
+    return null;
+  }
+
   return (
-   
-      <main className="flex-grow flex items-center justify-center min-h-screen">
-        <div className="flex flex-col items-center justify-center -mt-16">
+    <main className="flex-grow flex items-center justify-center min-h-screen">
+      <div className="flex flex-col items-center justify-center -mt-16">
         <h1 className="flex items-center justify-center text-5xl font-bold text-white animate-flipIn relative text-center">
           <span className="absolute inset-0 
             -translate-x-0.8
@@ -35,16 +46,17 @@ export default function Home() {
             transition-transform">
             Welcome to Employee Attrition Prediction
           </span>
-          </h1>
+        </h1>
 
-          <div className="flex justify-center items-center w-full mt-6">
-            <button className="mt-5 bg-blue-500 text-white font-bold px-8 py-4 rounded-full hover:bg-blue-700 transition-colors duration-150 shadow-lg"
-              onClick={handlePredictionClick}
-            >
+        <div className="flex justify-center items-center w-full mt-6">
+          <button 
+            className="mt-5 bg-blue-500 text-white font-bold px-8 py-4 rounded-full hover:bg-blue-700 transition-colors duration-150 shadow-lg"
+            onClick={handlePredictionClick}
+          >
             Go to Prediction   
-            </button>
-          </div>
+          </button>
         </div>
+      </div>
     </main>
   );
 }
